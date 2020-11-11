113ac397a0bd2b0ae1bb35704023799af8dc1507
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-11-11 12:43:52 +0530

    Tabs

--- Vue Components/main.js
+++ Vue Components/main.js
@@ -97,6 +97,71 @@ Vue.component('modal', {
     }
 });
 
+Vue.component('tabs', {
+    template: `
+<div>
+    <div class="tabs">
+        <ul class="nav nav-tabs">
+            <li v-for="tab in tabs"  :class="{'is-active':tab.isActive}">
+                <a  class="nav-link" :href="tab.href"  @click="selectTab(tab)">{{tab.name}}</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="tab-details">
+        <slot></slot>
+    </div>
+</div>      
+
+`,
+
+    data() {
+        return { tabs: [] }
+    },
+
+    created() {
+        this.tabs = this.$children;
+    },
+    methods: {
+        selectTab(selectedTab) {
+
+            //alert("selecting")
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            })
+        }
+    }
+});
+
+Vue.component('tab', {
+
+    template: `
+    <div v-show="isActive"><slot></slot></div>
+    `,
+
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data() {
+
+        return {
+
+            isActive: false
+        }
+
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    }
+})
+
 
 new Vue({
     el: '#root',
