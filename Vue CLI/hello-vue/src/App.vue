166a81602b38bf7b32fcf4c4a474e0d36d551cfc
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-11-12 11:38:04 +0530

    Vue CLI

---
+++ Vue CLI/hello-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <message>Hello Vue </message>
+  </div>
+</template>
+
+<script>
+
+import Message from './Components/Message.vue'
+
+export default {
+  name: 'app',
+  components:{Message},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
